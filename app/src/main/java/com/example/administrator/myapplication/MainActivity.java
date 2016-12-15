fd5384b871ec14320eead84f59406ac02de4b49d
acmelacross <www14159@163.com>
2016-12-15 14:38:30 +0800

    123

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.administrator.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * asd
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
